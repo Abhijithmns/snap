45852202647cf28dc282281001b51e6e8a972ee2
Abhijithmns <abhijithmns07@gmail.com>
2026-06-09 00:11:33 +0530

    add ppm creation and channel, cap_full()

--- snap.c
+++ snap.c
@@ -6,9 +6,20 @@
 #include <stdio.h>
 #include <sys/types.h>
 
+#define MIN(x,y) (x > y ? y : x)
+#define MAX(x,y) (x > y ? x : y)
+
 void cap_sel(void);
 void cap_win(void);
 void draw_rect(void);
+
+enum mode {
+    MODE_SEL,
+    MODE_FULL,
+    MODE_SCR,
+    MODE_WIN
+};
+
 
 void overlay(Display *dpy, Window window,unsigned int width, unsigned int height) {
     XSetWindowAttributes overlayattr;
@@ -25,7 +36,7 @@ void overlay(Display *dpy, Window window,unsigned int width, unsigned int height
       while(XPending(dpy) > 0) {
         XEvent event = {0};
         XNextEvent(dpy,&event);
-        if(event.type == KeyPress) {
+        if(event.type == KeyPress) { 
           quit = true;
         }
       }
@@ -35,14 +46,64 @@ void overlay(Display *dpy, Window window,unsigned int width, unsigned int height
 
 }
 
+
+unsigned char channel(unsigned long pxl, Mask m) {
+    if(m == 0) return 0;
+
+    int shift = 0;
+    //shift down until we find a channel
+    while((m & 1UL) == 0) {
+        m >>= 1UL;
+        shift++;
+    }
+
+    // no of bits in channel
+    int bits = 0;
+    while(m & 1UL) {
+        m >>= 1UL;
+        bits++;
+    }
+
+    //raw channel value
+    // shift pxl right to bring the channel to bit number '0' , then cut off upper bits
+    unsigned long rcv = (pxl >> shift) & ( (1UL << bits) - 1UL );
+
+    // scale from [0, 2^bits - 1] -> [0, 255] to meet standard
+    // so this maps 0->0 and 31 -> 255 
+    if(bits == 8) return rcv; // already 8-bits wide
+    return ((rcv * 255UL) / ((1UL << bits) - 1UL));
+}
+
+
+void mkppm(XImage *img) {
+    FILE *out = stdout;
+
+    // write ppm header metadata
+    fprintf(out, "P6\n%d %d\n255\n", img->width, img->height);
+
+    for(int i = 0; i < img->height; i++) {
+        for(int j = 0; j < img->width; j++) {
+            unsigned long pxl = XGetPixel(img, j, i);
+            fputc(channel(pxl, img->red_mask),out);
+            fputc(channel(pxl, img->green_mask),out);
+            fputc(channel(pxl, img->blue_mask),out);
+        }
+    }
+
+    fflush(stdout);
+}
+
 void cap_fullscr(Display *dpy,int scr,Window window) {
     unsigned int width = DisplayWidth(dpy, scr);
     unsigned int height = DisplayHeight(dpy, scr);
 
     XImage *img = XGetImage(dpy,window, 0, 0, width, height, AllPlanes, ZPixmap);
-    if(!img) printf("XGetImage failed");
+    if(!img) {
+        printf("XGetImage failed");
+        return;
+    }
     
-    // TODO : save or cpy the image based on the key press
+    mkppm(img);
 }
 
 
@@ -55,7 +116,7 @@ int main() {
     XGetWindowAttributes(display, rootWindow,&rootwinattr);
     unsigned int width = rootwinattr.width;
     unsigned int height = rootwinattr.height;
-    overlay(display, rootWindow, width, height);
+    cap_fullscr(display,DefaultScreen(display), rootWindow);
     XCloseDisplay(display);
     return 0;
 
