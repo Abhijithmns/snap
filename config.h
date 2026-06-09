e38440b34426d9d464d2912f3f3e33a780a3f1d6
Abhijithmns <abhijithmns07@gmail.com>
2026-06-09 18:51:11 +0530

    add config.h

---
+++ config.h
@@ -0,0 +1,6 @@
+#include <X11/cursorfont.h>
+#include <X11/Xlib.h>
+
+static const unsigned int cursor_font = XC_crosshair;
+static const unsigned int rect_line_width = 2;
+static const unsigned int rect_line_style = LineSolid;
